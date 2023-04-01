e1e2ef311fdff906f992338b3f87fd310612e84e
simo M <simo2@simos>
2023-04-01 14:51:26 -0700

    newOne

--- functionLab.js
+++ functionLab.js
@@ -1,4 +1,10 @@
-console.log('do')
-for(let i=0; i<5; i++){
-console.log(i)
+// Write a function called logMyName that will log your name.
+const logMyName = (name) => {
+console.log(name)
 }
+logMyName('Simo')
+// 2. Write a function called logThisName that will log the name parameter.
+const  logThisName = (name) => {
+    console.log(name)
+  }
+  logThisName("James")
